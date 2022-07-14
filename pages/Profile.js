90d2b9f7dea9a60a1fc7b5928a02494ad3462e2d
vnxa01 <vnxa01@gmail.com>
2022-07-14 12:40:26 +0700

    update profile

--- pages/Profile.js
+++ pages/Profile.js
@@ -16,7 +16,9 @@ export default function Profile() {
     const [image, setImage] = useState(null);
     const [url,setUrl]= useState(null)
     const [username,setUsername]= useState(null)
-    const [user,setUser] = useState(null)
+    const [userId,setUserId] = useState(null)
+    const [user,setUser] = useState()
+    const [avatar,setAvatar] = useState()
 
 
     const handleChange=(e)=>{
@@ -25,7 +27,7 @@ export default function Profile() {
         }
     }
     
-    const uploadFile=async ()=>{
+    const uploadFile= async ()=>{
         if(image==null)return;
         const imageRef = ref(storage, `images/${image.name + v4()}`);
         await uploadBytes(imageRef, image);
@@ -37,14 +39,13 @@ export default function Profile() {
     async function update(url,username){
         await uploadFile();
         let promise = Axios({
-            url: "http://localhost:5000/api/profile/update/"+user,
+            url: "http://localhost:5000/api/profile/update/"+userId,
             method: "PUT",
-            data: {image: url,username:username }
+            data: {image: url, username: username }
           });
           promise
             .then((result) => {
               console.log(result.data);
-              // console.log(this.state.taskId);
             })
             .catch((err) => {
               console.log(err.response.data);
@@ -53,12 +54,13 @@ export default function Profile() {
             setLogin(true)
     }
     const updateProfile = async()=>{
-
-        update(url,username);
+       await update(url,username);
     }
     
     async function login(address){
         let id =null
+        let user = null
+        let avatar = null;
         let promise = Axios({
             url: "http://localhost:5000/api/auth/login",
             method: "POST",
@@ -68,6 +70,11 @@ export default function Profile() {
             .then((result) => {
                 
                 id = result.data.user._id;
+                user= result.data.user.username
+                avatar = result.data.user.image
+                console.log(user);
+                setUser(user)
+                setAvatar(avatar)
               console.log(id);
             //   setUser(id)
             //   console.log(user)
@@ -76,7 +83,8 @@ export default function Profile() {
             .catch((err) => {
               console.log(err.response.data);
             });
-            setUser(id)
+            
+            setUserId(id)
             setLogin(true)
     }
     useEffect(()=> {
@@ -94,34 +102,30 @@ export default function Profile() {
         setAddress(signerAddress);
     }
 
-
-
-
   return (
-    <div>{
+    <div>
+      
+        {
         isLogin ?   <div className="w-full mx-auto max-w-xl rounded-lg bg-white dark:bg-gray-800 shadow-lg px-5 py-4 text-gray-800 dark:text-gray-50" style={{marginTop: 100}}>
         <div className="w-full pt-1 text-center -mt-16 mx-auto">
             <a href="#" className="block relative">
-            <img alt="avatar" src="https://vcdn1-sohoa.vnecdn.net/2021/12/20/bored-ape-nft-accidental-0-728-7234-6530-1639974498.jpg?w=1200&h=0&q=100&dpr=1&fit=crop&s=C624CYTwi01i3bZb6oNBEg" className="mx-auto object-cover rounded-full h-20 w-20 " />
+            <img alt="avatar" src={avatar} className="mx-auto object-cover rounded-full h-20 w-20 " />
             </a>
         </div>
         <div className="w-full">
             <div className="text-center mb-6">
                 <p className="text-gray-800 dark:text-white text-xl font-medium">
-                null
+                {user}
                 </p>
                 <p className="text-gray-400 text-s">
                     {address}
                 </p>
-
             </div>
                 <button type="button" className="py-2 px-4  bg-indigo-600 hover:bg-indigo-700 focus:ring-indigo-500 focus:ring-offset-indigo-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg ">
                 Update
                 </button>
                 
             </div>
-            
-           
          </div> :  <Login></Login>
         }
         
@@ -132,7 +136,6 @@ export default function Profile() {
             <button onClick={updateProfile} >Save</button>
            </div>
         </div>
-       
     </div>
      
         
